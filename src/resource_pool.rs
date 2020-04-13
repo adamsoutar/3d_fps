3ae8de2869ab7519cd83866b00ab059d61a34057
Adam <adam@overflo.me>
2020-04-13 12:25:43 +0100

    Ironically that previous commit broke texture loading

--- src/resource_pool.rs
+++ src/resource_pool.rs
@@ -19,6 +19,13 @@ pub fn create_and_load () -> ResourcePool {
     for path in paths {
         let pth = path.unwrap().path();
         let tex_name = pth.file_stem().unwrap().to_str().unwrap().to_owned();
+        let ext = pth.extension().unwrap();
+
+        if ext != "png" && ext != "jpg" && ext != "bmp" {
+            println!("Texture with extension {:?} wasn't loaded because it wasn't PNG, JPG or BMP.", ext);
+            continue;
+        }
+
         println!("Loading \"{}\"...", tex_name);
         let tex = image::open(&pth).unwrap().to_rgba();
 
